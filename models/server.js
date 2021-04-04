7b2705592235ec29a0c2a7a72917ec3a41816d0d
Luis Manestar <luismsnestar9@gmail.com>
2021-04-04 11:07:34 -0500

    Segundo commit

--- models/server.js
+++ models/server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const http =require('http');
 const socketio = require('socket.io');
 const path = require('path');
+const cors = require('cors');
 
 const Sockets = require('./sockets');
+
 
 
 class Server {
@@ -26,6 +28,9 @@ class Server {
     middlewares(){
         // desplegar el directorio publico
         this.app.use( express.static( path.resolve( __dirname, '../public') ));
+
+        //CORS
+        this.app.use( cors());
     }
 
     configurarSockets(){
